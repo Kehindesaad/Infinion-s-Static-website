59ccddea2543977061b67bcaa409d9dd9d150249
Olohungbebe kehinde Sa'adudeen <kehindesaad@gmail.com>
2024-06-04 00:00:24 +0100

    Made a change to the Output file

--- Terraform/output.tf
+++ Terraform/output.tf
@@ -1,7 +1,7 @@
 output "public_ip" {
-  value = azurerm_public_ip.example.ip_address
+  value = azurerm_public_ip.Demo-IP.ip_address
 }
 
 output "frontdoor_endpoint" {
-  value = azurerm_frontdoor.example.frontend_endpoint[0].host_name
+  value = azurerm_frontdoor.DemoFD.frontend_endpoint[0].host_name
 }
